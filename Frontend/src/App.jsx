018974f98ee0dd2b29c3a07a5f5c990228dbd4c7
NehaKaushal259 <nehakaushal392@gmail.com>
2026-03-22 22:46:16 +0530

    create folders

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind v4 Working 🚀
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
